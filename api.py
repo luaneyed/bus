0eef15c5ebef6a5fde2350433cf4d34289e02067
Luan Sangguk Lee <sangguk258@gmail.com>
2018-04-16 20:33:46 +0900

    Log error if occurred

--- api.py
+++ api.py
@@ -13,11 +13,15 @@ def call(service: str, params: dict):
     response_body = urlopen(request).read().decode('utf-8')
     xml_root = Et.fromstring(response_body)
 
-    # com_msg_header = xml_root.find('comMsgHeader')
+    com_msg_header = xml_root.find('comMsgHeader')
     msg_header = xml_root.find('msgHeader')
 
     return_code = msg_header.find('resultCode').text
     if return_code == '4':
+        return None
+
+    if return_code != '0':
+        print('Error occurred! {}'.format(com_msg_header.find('errMsg').text))
         return None
 
     return xml_root.find('msgBody')
